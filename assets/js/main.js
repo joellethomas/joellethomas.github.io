6f73e9368f044cc6accbe97a5e52f05433355f14
Joelle <jtho0009@student.monash.edu>
2019-06-13 13:20:13 +1000

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,15 @@ $( document ).ready(function() {
 	});
 
 });
+
+$( document ).ready(function() {
+  
+  $( ".book1, .book2, .book3, .book4, .book5, .book6, .book7, .book8, .book9, .book10, .book11, .book12, .book13, .book14, .book15, .book16, .book17, .book18, .book19, .book20, .book21, .book22, .book23, .book24, .book25, .book26, .book27, .book28, .book29, .book30, .book31" ).mouseenter(function() {
+  	$( this ).addClass("active");
+	});
+
+    $( ".book1, .book2, .book3, .book4, .book5, .book6, .book7, .book8, .book9, .book10, .book11, .book12, .book13, .book14, .book15, .book16, .book17, .book18, .book19, .book20, .book21, .book22, .book23, .book24, .book25, .book26, .book27, .book28, .book29, .book30, .book31" ).click(function() {
+  	$( this ).removeClass("active");
+	});
+
+});
