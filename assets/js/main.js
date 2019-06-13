b883eda4842cc8034bf95d7d18e5f1102e3b3353
Joelle <jtho0009@student.monash.edu>
2019-06-13 10:13:09 +1000

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
   
-  $( ".p1, .p2, .p3, .p4, .p5" ).mouseenter(function() {
+  $( ".p1, .p2, .p3, .p4, .p5, .p6, .p7, .p8, .p9, .p10, .p11, .p12" ).mouseenter(function() {
   	$( this ).addClass("active");
 	});
 
-    $( ".p1, .p2, .p3, .p4, .p5" ).click(function() {
+    $( ".p1, .p2, .p3, .p4, .p5, .p6, .p7, .p8, .p9, .p10, .p11, .p12" ).click(function() {
   	$( this ).removeClass("active");
 	});
 
