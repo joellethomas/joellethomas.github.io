1357593410ba6f9f59efb0034a4118c079efa723
Joelle <jtho0009@student.monash.edu>
2019-11-17 15:59:23 +1100

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,38 @@
 $( document ).ready(function() {
-  
-  $( ".p1, .p2, .p3, .p4, .p5, .p6, .p7, .p8, .p9, .p10, .p11, .p12, .p13, .p14, .p15, .p16, .p17, .p18, .p19, .p20, .p21, .p22, .p23, .p24, .p25, .p26, .p27, .p28, .p29, .g1, .g2, .g3, .g4, .g5, .g6, .g7, .g8, .g9, .g10" ).mouseenter(function() {
-  	$( this ).addClass("active");
-	});
-
-    $( ".p1, .p2, .p3, .p4, .p5, .p6, .p7, .p8, .p9, .p10, .p11, .p12, .p13, .p14, .p15, .p16, .p17, .p18, .p19, .p20, .p21, .p22, .p23, .p24, .p25, .p26, .p27, .p28, .p29, .g1, .g2, .g3, .g4, .g5, .g6, .g7, .g8, .g9, .g10" ).click(function() {
-  	$( this ).removeClass("active");
-	});
-
+// start here    
+$('.menu-link').click(function() {
+  $('.menu').addClass('left');
 });
 
-$( document ).ready(function() {
-  
-  $( ".book1, .book2, .book3, .book4, .book5, .book6, .book7, .book8, .book9, .book10, .book11, .book12, .book13, .book14, .book15, .book16, .book17, .book18, .book19, .book20, .book21, .book22, .book23, .book24, .book25, .book26, .book27, .book28, .book29, .book30, .book31" ).mouseenter(function() {
-  	$( this ).addClass("active");
-	});
+$('.close-link').click(function() {
+  $('.menu').removeClass('left');
+});
 
-    $( ".book1, .book2, .book3, .book4, .book5, .book6, .book7, .book8, .book9, .book10, .book11, .book12, .book13, .book14, .book15, .book16, .book17, .book18, .book19, .book20, .book21, .book22, .book23, .book24, .book25, .book26, .book27, .book28, .book29, .book30, .book31" ).click(function() {
-  	$( this ).removeClass("active");
-	});
+// popup link
 
+$('.popup-link').click(function() {
+  $('.popup').addClass('active');
+});
+
+	$('.popup').click(function() {
+  $('.popup').addClass('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end here
 });
---
+++ assets/js/toggle.js
@@ -0,0 +1,57 @@
+$( document ).ready(function() {
+    
+
+$( ".title-1" ).click(function() {
+  $( ".toggle-1" ).slideToggle( "slow" );
+});
+
+$( ".title-2" ).click(function() {
+  $( ".toggle-2" ).slideToggle( "slow" );
+});
+
+$( ".title-3" ).click(function() {
+  $( ".toggle-3" ).slideToggle( "slow" );
+});
+
+$( ".title-a" ).click(function() {
+  $( ".toggle-a" ).slideToggle( "slow" );
+});
+
+$( ".title-b" ).click(function() {
+  $( ".toggle-b" ).slideToggle( "slow" );
+});
+
+$( ".title-c" ).click(function() {
+  $( ".toggle-c" ).slideToggle( "slow" );
+});
+
+$( ".title-d" ).click(function() {
+  $( ".toggle-d" ).slideToggle( "slow" );
+});
+
+$( ".title-e" ).click(function() {
+  $( ".toggle-e" ).slideToggle( "slow" );
+});
+
+$( ".title-f" ).click(function() {
+  $( ".toggle-f" ).slideToggle( "slow" );
+});
+
+$( ".title-g" ).click(function() {
+  $( ".toggle-g" ).slideToggle( "slow" );
+});
+
+$( ".title-h" ).click(function() {
+  $( ".toggle-h" ).slideToggle( "slow" );
+});
+
+
+
+
+
+
+
+
+
+
+});
